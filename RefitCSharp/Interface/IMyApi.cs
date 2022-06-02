32bb909ebfb08cabff7dbeeb9d92363fd44dafba
Eshat ÇELİK <eshat.celik@procenne.com>
2022-06-02 10:46:29 +0300

    Add project files.

---
+++ RefitCSharp/Interface/IMyApi.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+using RefitCSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefitCSharp.Interface
+{
+    public interface IMyApi
+    {
+        [Get("/todos")]
+        Task<List<Todos>> GetTodos();
+    }
+}
---
+++ RefitCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Refit;
+using RefitCSharp.Interface;
+using RefitCSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RefitCSharp
+{
+    class Program
+    {
+        public static async Task<List<Todos>> GetTodosAsync()
+        {
+            List<Todos> todos = new List<Todos>();
+            var myApi = RestService.For<IMyApi>("http://jsonplaceholder.typicode.com");
+
+            return await myApi.GetTodos();
+        }
+        static async Task  Main(string[] args)
+        {
+            var todos =await GetTodosAsync();
+            foreach (var item in todos)
+            {
+                Console.WriteLine("Id: "+item.id+" -"+item.title);
+            }
+            Console.ReadKey();
+        }
+    }
+}
